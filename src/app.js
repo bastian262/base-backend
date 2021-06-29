ed55e6d1e14f3f6250f9213c6854ab1144c89410
bastianupcompany <bastian@upcompany.cl>
2021-06-29 03:10:41 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const Server = require('./models/server');
 
 try{
-
     const server = new Server();
-    
-    
-    
+
     server.listen();
+
 }catch(err){
     throw new Error(err);
 }
