f58f0458653b38f5f8ac080467fd9a41cda96c96
bastianupcompany <bastian@upcompany.cl>
2021-06-29 02:11:33 -0400

    Update usuarios.js

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -15,16 +15,19 @@ const Usuario = require('../models/usuario')
 const usuariosGet = async (req = request, res = response) => {
 
     // const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
-    const { limite = 1, desde = 0 } = req.query;
-
-    const user = await Usuario.find()
-    .skip(Number(desde));
-
-    res.json({
-        ok:true,
-        msg: 'Listado correctamente',
-        user
-    });
+    const { desde = 0 } = req.query;
+    try{
+        const user = await Usuario.find()
+        .skip(Number(desde));
+    
+        res.json({
+            ok:true,
+            msg: 'Listado correctamente',
+            user
+        });
+    }catch(err){
+        throw err;
+    }
 }
 
 const usuariosPost = async (req, res = response) => {
@@ -32,41 +35,45 @@ const usuariosPost = async (req, res = response) => {
     const {nombre, correo, telefono} = req.body;
     const user = new Usuario({nombre, telefono, correo});
     user.rol = "USER_ROLE";
-    await user.save((err, userStored) => {
-        if(err){
-            res.status(400).send({ok:fakse, message:"El correo ya fue registrado"});
-        }else{
-            if(!userStored){
-                res.status(500).send({ ok: false, message: "Error al crear el usuario" });
+    try{
+        await user.save((err, userStored) => {
+            if(err){
+                res.status(400).send({ok:fakse, message:"El correo ya fue registrado"});
             }else{
-                // const mailOptions = {
-                //     from: `Health Tech Latam <${stulzelEmail}>`,
-                //     to: userStored.email,
-                //     subject: 'Prueba mail',
-                //     text: 'PruebaMail',
-                //     html: `
-                //     <html>
-                //         <head>
-                //             <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap" rel="stylesheet">
-                //             <title>Stulzel!</title>
-                //         </head>
-                //         <body style="background:#f6f6f6;">
-                           
-                //             <h1> PRUEBA </h1>
-                //         </body>
-                //     </html>
-                //     `
-                // };
-                // transporter.sendMail(mailOptions, function(error, info){
-                //     if(error){
-                //         res.status(500).send({ ok: false, message: "Error del servidor"});
-                //     } else {
-                        res.status(200).send({ ok: true, message: "usuario Agregado correctamente ", user: user });
-                //     }
-                // });
+                if(!userStored){
+                    res.status(500).send({ ok: false, message: "Error al crear el usuario" });
+                }else{
+                    // const mailOptions = {
+                    //     from: `Health Tech Latam <${stulzelEmail}>`,
+                    //     to: userStored.email,
+                    //     subject: 'Prueba mail',
+                    //     text: 'PruebaMail',
+                    //     html: `
+                    //     <html>
+                    //         <head>
+                    //             <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+                    //             <title>Stulzel!</title>
+                    //         </head>
+                    //         <body style="background:#f6f6f6;">
+                               
+                    //             <h1> PRUEBA </h1>
+                    //         </body>
+                    //     </html>
+                    //     `
+                    // };
+                    // transporter.sendMail(mailOptions, function(error, info){
+                    //     if(error){
+                    //         res.status(500).send({ ok: false, message: "Error del servidor"});
+                    //     } else {
+                            res.status(200).send({ ok: true, message: "usuario Agregado correctamente ", user: user });
+                    //     }
+                    // });
+                }
             }
-        }
-    });
+        });
+    }catch(err){
+        throw err;
+    }
 }
 
 // const usuariosPut = async (req, res = response) => {
@@ -98,12 +105,17 @@ const usuariosPost = async (req, res = response) => {
 const usuariosDelete = async (req, res = response) => {
 
     const { id } = req.params;
+    try{
+        const user = await Usuario.findByIdAndDelete(id);
+        res.json({
+            msg: 'Usuario Eliminado Correctamente!'
+        });
 
-    const user = await Usuario.findByIdAndDelete(id);
+    
+    }catch(err){
+        throw err;
+    }
 
-    res.json({
-        msg: 'Usuario Eliminado Correctamente!'
-    });
 }
 
 
