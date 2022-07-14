328ef73863e7f55016dc658eeece6df79b878eb0
bastian262 <80422192+bastian262@users.noreply.github.com>
2022-07-14 03:06:59 -0400

    Update sendMail.js

--- src/controllers/sendMail.js
+++ src/controllers/sendMail.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 const { emailGira } = require("../utils/templates");
-const stulzelEmail = process.env.USERMAIL;
-const passwordEmail = process.env.PASSWORDMAIL;
+const stulzelEmail = "mailingstulzel1@gmail.com";
+const passwordEmail = "Stulzel385#";
 
 const transporter = nodemailer.createTransport({
     servide: "Gmail",
@@ -17,21 +17,24 @@ const transporter = nodemailer.createTransport({
 
 const sendMailGira = (req, res) => {
     const { url, email, name } = req.body
+    try {
+        const mailOptions = {
+            from: `STULZEL ${stulzelEmail}}`,
+            to: email,
+            subject: "Gira Stulzel",
+            text: "Gracias por inscribirte en esta gira de stulzel",
+            html: emailGira(name, url)
+        }
 
-    const mailOptions = {
-        from: `STULZEL ${stulzelEmail}}`,
-        to: email,
-        subject: "Gira Stulzel",
-        text: "Gracias por inscribirte en esta gira de stulzel",
-        html: emailGira(name, url)
+        transporter.sendMail(mailOptions, (err, info) => {
+            console.log(err)
+            console.log(info)
+            if (err) return res.status(500).json({ ok: false, message: "Error al enviar correo", err })
+            return res.status(200).json({ ok: true, message: "Correo enviado exitosamente!" })
+        })
+    } catch (err) {
+        return res.status(500).json({ ok: false, message: "Error al enviar correo", err })
     }
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        console.log(err)
-        console.log(info)
-        if (err) return res.status(500).json({ ok: false, message: "Error al enviar correo" })
-        return res.status(200).json({ ok: true, message: "Correo enviado exitosamente!" })
-    })
 
 }
 
